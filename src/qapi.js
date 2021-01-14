46ed8e512478a046d6e612bad3c4a509d439ead1
hypertrendco <32074330+hypertrendco@users.noreply.github.com>
2021-01-13 22:44:45 -0500

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fffbd6dd97277001a088a94",
+				"5fffbd6dd97277001a088a96"
+			]
+		},
+		"5fffbd6dd97277001a088a94": {
+			"id": "5fffbd6dd97277001a088a94",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fffbd6dd97277001a088a96": {
+			"id": "5fffbd6dd97277001a088a96",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
